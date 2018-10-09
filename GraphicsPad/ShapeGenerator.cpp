143f818a1bdd10ae4c75a502fc51ba6179de7428
Alex Hrazhdan <shkval3d@gmail.com>
2018-10-09 18:01:44 +0300

    add some changes

--- GraphicsPad/ShapeGenerator.cpp
+++ GraphicsPad/ShapeGenerator.cpp
@@ -150,123 +150,123 @@ ShapeData ShapeGenerator::makeArrow()
 		vec3(-0.50f, +0.25f, -0.25f),         // 3
 		vec3(+1.00f, +0.00f, +0.00f),		  // Color
 		vec3(+0.00f, +1.00f, +0.00f),         // Normal
-											  // Bottom side of arrow head
-											  vec3(+0.00f, -0.25f, -0.25f),         // 4
-											  vec3(+0.00f, +0.00f, +1.00f),		  // Color
-											  vec3(+0.00f, -1.00f, +0.00f),         // Normal
-											  vec3(+0.50f, -0.25f, -0.25f),         // 5
-											  vec3(+0.00f, +0.00f, +1.00f),		  // Color
-											  vec3(+0.00f, -1.00f, +0.00f),         // Normal
-											  vec3(+0.00f, -0.25f, -1.00f),         // 6
-											  vec3(+0.00f, +0.00f, +1.00f),		  // Color
-											  vec3(+0.00f, -1.00f, +0.00f),         // Normal
-											  vec3(-0.50f, -0.25f, -0.25f),         // 7
-											  vec3(+0.00f, +0.00f, +1.00f),		  // Color
-											  vec3(+0.00f, -1.00f, +0.00f),         // Normal
-																					// Right side of arrow tip
-																					vec3(+0.50f, +0.25f, -0.25f),         // 8
-																					vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																					vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
-																					vec3(+0.00f, +0.25f, -1.00f),         // 9
-																					vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																					vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
-																					vec3(+0.00f, -0.25f, -1.00f),         // 10
-																					vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																					vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
-																					vec3(+0.50f, -0.25f, -0.25f),         // 11
-																					vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																					vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
-																															// Left side of arrow tip
-																															vec3(+0.00f, +0.25f, -1.00f),         // 12
-																															vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																															vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
-																															vec3(-0.50f, +0.25f, -0.25f),         // 13
-																															vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																															vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
-																															vec3(+0.00f, -0.25f, -1.00f),         // 14
-																															vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																															vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
-																															vec3(-0.50f, -0.25f, -0.25f),         // 15
-																															vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																															vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
-																																									 // Back side of arrow tip
-																																									 vec3(-0.50f, +0.25f, -0.25f),         // 16
-																																									 vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																									 vec3(+0.00f, +0.00f, +1.00f),         // Normal
-																																									 vec3(+0.50f, +0.25f, -0.25f),         // 17
-																																									 vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																									 vec3(+0.00f, +0.00f, +1.00f),         // Normal
-																																									 vec3(-0.50f, -0.25f, -0.25f),         // 18
-																																									 vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																									 vec3(+0.00f, +0.00f, +1.00f),         // Normal
-																																									 vec3(+0.50f, -0.25f, -0.25f),         // 19
-																																									 vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																									 vec3(+0.00f, +0.00f, +1.00f),         // Normal
-																																																		   // Top side of back of arrow
-																																																		   vec3(+0.25f, +0.25f, -0.25f),         // 20
-																																																		   vec3(+1.00f, +0.00f, +0.00f),		  // Color
-																																																		   vec3(+0.00f, +1.00f, +0.00f),         // Normal
-																																																		   vec3(+0.25f, +0.25f, +1.00f),         // 21
-																																																		   vec3(+1.00f, +0.00f, +0.00f),		  // Color
-																																																		   vec3(+0.00f, +1.00f, +0.00f),         // Normal
-																																																		   vec3(-0.25f, +0.25f, +1.00f),         // 22
-																																																		   vec3(+1.00f, +0.00f, +0.00f),		  // Color
-																																																		   vec3(+0.00f, +1.00f, +0.00f),         // Normal
-																																																		   vec3(-0.25f, +0.25f, -0.25f),         // 23
-																																																		   vec3(+1.00f, +0.00f, +0.00f),		  // Color
-																																																		   vec3(+0.00f, +1.00f, +0.00f),         // Normal
-																																																												 // Bottom side of back of arrow
-																																																												 vec3(+0.25f, -0.25f, -0.25f),         // 24
-																																																												 vec3(+0.00f, +0.00f, +1.00f),		  // Color
-																																																												 vec3(+0.00f, -1.00f, +0.00f),         // Normal
-																																																												 vec3(+0.25f, -0.25f, +1.00f),         // 25
-																																																												 vec3(+0.00f, +0.00f, +1.00f),		  // Color
-																																																												 vec3(+0.00f, -1.00f, +0.00f),         // Normal
-																																																												 vec3(-0.25f, -0.25f, +1.00f),         // 26
-																																																												 vec3(+0.00f, +0.00f, +1.00f),		  // Color
-																																																												 vec3(+0.00f, -1.00f, +0.00f),         // Normal
-																																																												 vec3(-0.25f, -0.25f, -0.25f),         // 27
-																																																												 vec3(+0.00f, +0.00f, +1.00f),		  // Color
-																																																												 vec3(+0.00f, -1.00f, +0.00f),         // Normal
-																																																																					   // Right side of back of arrow
-																																																																					   vec3(+0.25f, +0.25f, -0.25f),         // 28
-																																																																					   vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																																																																					   vec3(+1.00f, +0.00f, +0.00f),         // Normal
-																																																																					   vec3(+0.25f, -0.25f, -0.25f),         // 29
-																																																																					   vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																																																																					   vec3(+1.00f, +0.00f, +0.00f),         // Normal
-																																																																					   vec3(+0.25f, -0.25f, +1.00f),         // 30
-																																																																					   vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																																																																					   vec3(+1.00f, +0.00f, +0.00f),         // Normal
-																																																																					   vec3(+0.25f, +0.25f, +1.00f),         // 31
-																																																																					   vec3(+0.60f, +1.00f, +0.00f),		  // Color
-																																																																					   vec3(+1.00f, +0.00f, +0.00f),         // Normal
-																																																																															 // Left side of back of arrow
-																																																																															 vec3(-0.25f, +0.25f, -0.25f),         // 32
-																																																																															 vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																																																																															 vec3(-1.00f, +0.00f, +0.00f),         // Normal
-																																																																															 vec3(-0.25f, -0.25f, -0.25f),         // 33
-																																																																															 vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																																																																															 vec3(-1.00f, +0.00f, +0.00f),         // Normal
-																																																																															 vec3(-0.25f, -0.25f, +1.00f),         // 34
-																																																																															 vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																																																																															 vec3(-1.00f, +0.00f, +0.00f),         // Normal
-																																																																															 vec3(-0.25f, +0.25f, +1.00f),         // 35
-																																																																															 vec3(+0.00f, +1.00f, +0.00f),		  // Color
-																																																																															 vec3(-1.00f, +0.00f, +0.00f),         // Normal
-																																																																																								   // Back side of back of arrow
-																																																																																								   vec3(-0.25f, +0.25f, +1.00f),         // 36
-																																																																																								   vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																																																																								   vec3(+0.00f, +0.00f, +1.00f),         // Normal
-																																																																																								   vec3(+0.25f, +0.25f, +1.00f),         // 37
-																																																																																								   vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																																																																								   vec3(+0.00f, +0.00f, +1.00f),         // Normal
-																																																																																								   vec3(-0.25f, -0.25f, +1.00f),         // 38
-																																																																																								   vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																																																																								   vec3(+0.00f, +0.00f, +1.00f),         // Normal
-																																																																																								   vec3(+0.25f, -0.25f, +1.00f),         // 39
-																																																																																								   vec3(+0.50f, +0.50f, +0.50f),		  // Color
-																																																																																								   vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		// Bottom side of arrow head
+		vec3(+0.00f, -0.25f, -0.25f),         // 4
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		vec3(+0.50f, -0.25f, -0.25f),         // 5
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		vec3(+0.00f, -0.25f, -1.00f),         // 6
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		vec3(-0.50f, -0.25f, -0.25f),         // 7
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		// Right side of arrow tip
+		vec3(+0.50f, +0.25f, -0.25f),         // 8
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
+		vec3(+0.00f, +0.25f, -1.00f),         // 9
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
+		vec3(+0.00f, -0.25f, -1.00f),         // 10
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
+		vec3(+0.50f, -0.25f, -0.25f),         // 11
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(0.83205032f, 0.00f, -0.55470026f), // Normal
+		// Left side of arrow tip
+		vec3(+0.00f, +0.25f, -1.00f),         // 12
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
+		vec3(-0.50f, +0.25f, -0.25f),         // 13
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
+		vec3(+0.00f, -0.25f, -1.00f),         // 14
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
+		vec3(-0.50f, -0.25f, -0.25f),         // 15
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-0.55708605f, 0.00f, -0.37139067f), // Normal
+		// Back side of arrow tip
+		vec3(-0.50f, +0.25f, -0.25f),         // 16
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		vec3(+0.50f, +0.25f, -0.25f),         // 17
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		vec3(-0.50f, -0.25f, -0.25f),         // 18
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		vec3(+0.50f, -0.25f, -0.25f),         // 19
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		// Top side of back of arrow
+		vec3(+0.25f, +0.25f, -0.25f),         // 20
+		vec3(+1.00f, +0.00f, +0.00f),		  // Color
+		vec3(+0.00f, +1.00f, +0.00f),         // Normal
+		vec3(+0.25f, +0.25f, +1.00f),         // 21
+		vec3(+1.00f, +0.00f, +0.00f),		  // Color
+		vec3(+0.00f, +1.00f, +0.00f),         // Normal
+		vec3(-0.25f, +0.25f, +1.00f),         // 22
+		vec3(+1.00f, +0.00f, +0.00f),		  // Color
+		vec3(+0.00f, +1.00f, +0.00f),         // Normal
+		vec3(-0.25f, +0.25f, -0.25f),         // 23
+		vec3(+1.00f, +0.00f, +0.00f),		  // Color
+		vec3(+0.00f, +1.00f, +0.00f),         // Normal
+		// Bottom side of back of arrow
+		vec3(+0.25f, -0.25f, -0.25f),         // 24
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		vec3(+0.25f, -0.25f, +1.00f),         // 25
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		vec3(-0.25f, -0.25f, +1.00f),         // 26
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		vec3(-0.25f, -0.25f, -0.25f),         // 27
+		vec3(+0.00f, +0.00f, +1.00f),		  // Color
+		vec3(+0.00f, -1.00f, +0.00f),         // Normal
+		// Right side of back of arrow
+		vec3(+0.25f, +0.25f, -0.25f),         // 28
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(+1.00f, +0.00f, +0.00f),         // Normal
+		vec3(+0.25f, -0.25f, -0.25f),         // 29
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(+1.00f, +0.00f, +0.00f),         // Normal
+		vec3(+0.25f, -0.25f, +1.00f),         // 30
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(+1.00f, +0.00f, +0.00f),         // Normal
+		vec3(+0.25f, +0.25f, +1.00f),         // 31
+		vec3(+0.60f, +1.00f, +0.00f),		  // Color
+		vec3(+1.00f, +0.00f, +0.00f),         // Normal
+		// Left side of back of arrow
+		vec3(-0.25f, +0.25f, -0.25f),         // 32
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-1.00f, +0.00f, +0.00f),         // Normal
+		vec3(-0.25f, -0.25f, -0.25f),         // 33
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-1.00f, +0.00f, +0.00f),         // Normal
+		vec3(-0.25f, -0.25f, +1.00f),         // 34
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-1.00f, +0.00f, +0.00f),         // Normal
+		vec3(-0.25f, +0.25f, +1.00f),         // 35
+		vec3(+0.00f, +1.00f, +0.00f),		  // Color
+		vec3(-1.00f, +0.00f, +0.00f),         // Normal
+		// Back side of back of arrow
+		vec3(-0.25f, +0.25f, +1.00f),         // 36
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		vec3(+0.25f, +0.25f, +1.00f),         // 37
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		vec3(-0.25f, -0.25f, +1.00f),         // 38
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
+		vec3(+0.25f, -0.25f, +1.00f),         // 39
+		vec3(+0.50f, +0.50f, +0.50f),		  // Color
+		vec3(+0.00f, +0.00f, +1.00f),         // Normal
 	};
 
 	GLushort stackIndices[] = {
